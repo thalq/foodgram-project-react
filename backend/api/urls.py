7e87a4b42cbc3a9d909a3783541889f5c7a3308b
Татьяна Халквист <izvestiya9@mail.ru>
2022-07-05 14:44:33 +0300

    added api user

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+app_name = 'api'
+
+router_v1 = DefaultRouter()
+router_v1.register('users', UserViewSet, 'users')
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+]
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'users.apps.UsersConfig',
 ]
 
