ac86f2bbd74f2dd5fc8881ddf0c08b16e0b81402
Татьяна Халквист <izvestiya9@mail.ru>
2022-07-10 15:48:04 +0300

    дабвила в юзер-сериализатор create без нее постман не выдавал токен, хотя и создавал юзера

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -13,7 +13,19 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password')
+        extra_kwargs = {'password': {'write_only':True}}
+    
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class TagSerializer(serializers.ModelSerializer):
