37bb1515a4d4e76d5678721363afc4ff53257eba
Татьяна Халквист <izvestiya9@mail.ru>
2022-07-10 15:09:39 +0300

    Добавила проверку на цвет HEX

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,9 @@
-from dataclasses import field
-from operator import mod
-from statistics import mode
+from string import hexdigits
+
 from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
 
 from recipes.models import Tag
 
@@ -21,3 +21,14 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id', 'name', 'slug', 'color')
         read_only_fields = ('id', 'name', 'slug', 'color')
+
+    def validate_color(self, color):
+        color = str(color).strip('#')
+        if len(color) not in (3,6):
+            raise ValidationError(
+                f'Длина должна быть 3 или 6 символов, а не {len(color)}'
+            )
+        if not set(color).issubset(hexdigits):
+            raise ValidationError(
+                'Строка содержит недопустимый символ'
+            )
