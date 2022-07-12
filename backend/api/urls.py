df8e33eb75a5d0fbb6f3ba731b50246f30fcf60c
Татьяна Халквист <izvestiya9@mail.ru>
2022-07-12 10:31:15 +0300

    подключила ингредиенты к урлам

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, TagViewSet
+from .views import UserViewSet, TagViewSet, IngredientViewSet
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, 'users')
 router_v1.register('tags', TagViewSet, 'tags')
+router_v1.register('ingredients', IngredientViewSet, 'ingredients')
+
 
 urlpatterns = [
     path('', include(router_v1.urls)),
