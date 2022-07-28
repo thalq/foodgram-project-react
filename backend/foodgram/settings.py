794b74a764e9a11cc1e403249a60444c604376ce
Татьяна Халквист <izvestiya9@mail.ru>
2022-07-28 15:45:31 +0300

    fix static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -81,7 +81,7 @@ if SQLITE:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
