c918ed7bac254ff9bc50427b24faaa751cb92ee0
Татьяна Халквист <izvestiya9@mail.ru>
2022-07-28 15:43:43 +0300

    fix static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -81,7 +81,7 @@ if SQLITE:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
@@ -147,7 +147,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statis')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
